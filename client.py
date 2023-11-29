e06304c83a4e9dbd12065487df3c58897db89368
Jacob Funk <jacobstevenfunk@gmail.com>
2023-11-28 19:31:08 -0800

    created continuous polling for client

--- client.py
+++ client.py
@@ -1,12 +1,15 @@
 import requests
+import time
 
 raspberry_pi_ip = '10.0.0.49'  # Replace with your Raspberry Pi's IP address
 port = 8000
-
 url = f'http://{raspberry_pi_ip}:{port}'
 
-try:
-    response = requests.get(url)
-    print(f"Response from server: {response.text}")
-except Exception as e:
-    print(f"Error connecting to server: {e}")
+while True:
+    try:
+        response = requests.get(url)
+        print(f"Response from server: {response.text}")
+    except Exception as e:
+        print(f"Error connecting to server: {e}")
+    
+    time.sleep(1)  # Wait for 1 second between requests
--- server.py
+++ server.py
@@ -1,14 +1,20 @@
 import http.server
 import socketserver
+import time
 
 PORT = 8000
+counter = 0
 
 class SimpleHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
+        global counter
+        counter += 1  # Increment the counter with each request
+        message = f"Request number: {counter}"
+
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(b"Hello, you are connected to the Raspberry Pi HTTP server!")
+        self.wfile.write(message.encode())
 
 with socketserver.TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd:
     print(f"Server started at localhost:{PORT}")
