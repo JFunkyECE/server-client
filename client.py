06a6225f1c840be5dd8ed494ad4c2545f6b199d2
Jacob Funk <jacobstevenfunk@gmail.com>
2023-11-28 20:05:51 -0800

    added sending over csv file and reincluded counter for more verbose testing

--- client.py
+++ client.py
@@ -1,22 +1,65 @@
 import requests
 import os
+import time
 
-raspberry_pi_ip = '10.0.0.49'  # Replace with your Raspberry Pi's IP address
+raspberry_pi_ip = '10.0.0.49'  # Raspberry Pi's IP address
 port = 8000
-url = f'http://{raspberry_pi_ip}:{port}'
-image_url = f'{url}/image'
+base_url = f'http://{raspberry_pi_ip}:{port}'
+image_url = f'{base_url}/image'
+csv_url = f'{base_url}/csv'
 
-desktop_path = os.path.expanduser('~/Desktop')  # Expands the ~ to the full path of the user's home directory
+desktop_path = os.path.expanduser('~/Desktop')
 image_path = os.path.join(desktop_path, 'downloaded_image.jpg')
+csv_path = os.path.join(desktop_path, 'downloaded_file.csv')
 
-# Requesting and saving the image
-try:
-    response = requests.get(image_url)
-    if response.status_code == 200:
-        with open(image_path, 'wb') as file:
-            file.write(response.content)
-        print(f"Image saved as '{image_path}'")
-    else:
-        print("Failed to retrieve image")
-except Exception as e:
-    print(f"Error connecting to server: {e}")
+def request_counter():
+    try:
+        response = requests.get(base_url)
+        print(f"Counter response: {response.text}")
+    except Exception as e:
+        print(f"Error requesting counter: {e}")
+
+def request_image():
+    try:
+        response = requests.get(image_url)
+        if response.status_code == 200:
+            with open(image_path, 'wb') as file:
+                file.write(response.content)
+            print(f"Image saved as '{image_path}'")
+        else:
+            print("Failed to retrieve image")
+    except Exception as e:
+        print(f"Error requesting image: {e}")
+
+def request_csv():
+    try:
+        response = requests.get(csv_url)
+        if response.status_code == 200:
+            with open(csv_path, 'wb') as file:
+                file.write(response.content)
+            print(f"CSV file saved as '{csv_path}'")
+        else:
+            print("Failed to retrieve CSV file")
+    except Exception as e:
+        print(f"Error requesting CSV file: {e}")
+
+def main():
+    # Request counter a few times
+    for _ in range(3):
+        request_counter()
+        time.sleep(1)
+
+    # Request image
+    request_image()
+    time.sleep(1)
+
+    # Request counter a few more times
+    for _ in range(2):
+        request_counter()
+        time.sleep(1)
+
+    # Request CSV file
+    request_csv()
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -4,20 +4,32 @@ import os
 
 PORT = 8000
 image_path = os.path.expanduser('~/Desktop/spongebob.jpeg')
+csv_path = os.path.expanduser('~/Desktop/GPS_Data.csv')  # Replace with your CSV file path
 
 class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/image':
-            self.send_response(200)
-            self.send_header('Content-type', 'image/jpeg')
-            self.end_headers()
-            with open(image_path, 'rb') as file:
-                self.wfile.write(file.read())
+            self.serve_file(image_path, 'image/jpeg')
+        elif self.path == '/csv':
+            self.serve_file(csv_path, 'text/csv')
         else:
-            self.send_response(404)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write(b"Resource not found.")
+            self.serve_not_found()
+
+    def serve_file(self, file_path, content_type):
+        try:
+            with open(file_path, 'rb') as file:
+                self.send_response(200)
+                self.send_header('Content-type', content_type)
+                self.end_headers()
+                self.wfile.write(file.read())
+        except FileNotFoundError:
+            self.serve_not_found()
+
+    def serve_not_found(self):
+        self.send_response(404)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"Resource not found.")
 
 with socketserver.TCPServer(("", PORT), CustomHTTPRequestHandler) as httpd:
     print(f"Server started at localhost:{PORT}")
