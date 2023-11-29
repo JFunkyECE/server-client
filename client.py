e17f9414e0d1b6ed445e1af2a56eaec4c2928431
Jacob Funk <jacobstevenfunk@gmail.com>
2023-11-28 19:37:34 -0800

    Starting to add image transfer functionality

--- client.py
+++ client.py
@@ -1,15 +1,18 @@
 import requests
-import time
 
 raspberry_pi_ip = '10.0.0.49'  # Replace with your Raspberry Pi's IP address
 port = 8000
 url = f'http://{raspberry_pi_ip}:{port}'
+image_url = f'{url}/image'
 
-while True:
-    try:
-        response = requests.get(url)
-        print(f"Response from server: {response.text}")
-    except Exception as e:
-        print(f"Error connecting to server: {e}")
-    
-    time.sleep(1)  # Wait for 1 second between requests
+# Requesting and saving the image
+try:
+    response = requests.get(image_url)
+    if response.status_code == 200:
+        with open('downloaded_image.jpg', 'wb') as file:
+            file.write(response.content)
+        print("Image saved as 'downloaded_image.jpg'")
+    else:
+        print("Failed to retrieve image")
+except Exception as e:
+    print(f"Error connecting to server: {e}")
--- server.py
+++ server.py
@@ -1,12 +1,16 @@
 import http.server
 import socketserver
-import time
 
 PORT = 8000
-counter = 0
 
 class SimpleHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
+        if self.path == '/image':
+            self.serve_image()
+        else:
+            self.serve_counter()
+
+    def serve_counter(self):
         global counter
         counter += 1  # Increment the counter with each request
         message = f"Request number: {counter}"
@@ -15,6 +19,16 @@ class SimpleHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         self.wfile.write(message.encode())
+
+    def serve_image(self):
+        try:
+            with open("path/to/your/image.jpg", "rb") as file:  # Replace with your image path
+                self.send_response(200)
+                self.send_header('Content-type', 'image/jpeg')
+                self.end_headers()
+                self.wfile.write(file.read())
+        except FileNotFoundError:
+            self.send_error(404, "File Not Found")
 
 with socketserver.TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd:
     print(f"Server started at localhost:{PORT}")
