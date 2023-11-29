82036dbd5e613dddf238ed887b5db9ebc5011e11
Jacob Funk <jacobstevenfunk@gmail.com>
2023-11-28 20:49:05 -0800

    added code for gps-polling into server request handler and client

--- client.py
+++ client.py
@@ -5,9 +5,10 @@ import time
 raspberry_pi_ip = '10.0.0.49'  # Raspberry Pi's IP address
 port = 8000
 base_url = f'http://{raspberry_pi_ip}:{port}'
-counter_url = f'{base_url}/counter'  # Updated for counter requests
+counter_url = f'{base_url}/counter'  # For counter requests
 image_url = f'{base_url}/image'
 csv_url = f'{base_url}/csv'
+gps_poll_url = f'{base_url}/gps-poll'  # For GPS polling
 
 desktop_path = os.path.expanduser('~/Desktop')
 image_path = os.path.join(desktop_path, 'downloaded_image.jpg')
@@ -44,11 +45,22 @@ def request_csv():
     except Exception as e:
         print(f"Error requesting CSV file: {e}")
 
+def request_gps_poll():
+    try:
+        response = requests.get(gps_poll_url)
+        print("GPS polling started.")
+    except Exception as e:
+        print(f"Error starting GPS polling: {e}")
+
 def main():
     # Request counter a few times
     for _ in range(3):
         request_counter()
         time.sleep(1)
+
+    # Start GPS polling
+    request_gps_poll()
+    time.sleep(1)
 
     # Request image
     request_image()
--- server.py
+++ server.py
@@ -1,24 +1,23 @@
 import http.server
 import socketserver
 import os
+import threading
+import gpsd
+import time
+import csv
 
 PORT = 8000
 image_path = os.path.expanduser('~/Desktop/spongebob.jpeg')
-csv_path = os.path.expanduser('~/Desktop/GPS_Data.csv')  # Update this to your CSV file path
-
-counter = 0  # Initialize counter
+csv_path = os.path.expanduser('~/Desktop/GPS_Data.csv')
 
 class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        global counter
-
         if self.path == '/image':
             self.serve_file(image_path, 'image/jpeg')
         elif self.path == '/csv':
             self.serve_file(csv_path, 'text/csv')
-        elif self.path == '/counter':
-            counter += 1  # Increment the counter
-            self.serve_counter()
+        elif self.path == '/gps-poll':
+            self.serve_gps_poll()
         else:
             self.serve_not_found()
 
@@ -32,18 +31,49 @@ class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         except FileNotFoundError:
             self.serve_not_found()
 
-    def serve_counter(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        message = f"Counter value: {counter}"
-        self.wfile.write(message.encode())
-
     def serve_not_found(self):
         self.send_response(404)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         self.wfile.write(b"Resource not found.")
+
+    def serve_gps_poll(self):
+        thread = threading.Thread(target=self.poll_gps)
+        thread.start()
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"GPS polling started...")
+
+    def poll_gps(self):
+        gpsd.connect()
+
+        # Check if the file exists and if not, write the header
+        file_exists = os.path.exists(csv_path)
+
+        with open(csv_path, mode='a', newline='') as file:
+            writer = csv.writer(file)
+            
+            if not file_exists:
+                writer.writerow(["Latitude", "Longitude", "Time"])
+
+            while True:
+                try:
+                    packet = gpsd.get_current()
+                    if packet.mode >= 2:
+                        gps_time = packet.get_time(local_time=False)
+                        latitude = packet.lat
+                        longitude = packet.lon
+                        writer.writerow([latitude, longitude, gps_time])
+                        print(f"Written to CSV: Latitude: {latitude}, Longitude: {longitude}, Time: {gps_time}")
+                    else:
+                        print("Waiting for a valid GPS fix...")
+                    time.sleep(1)
+                except KeyError:
+                    continue
+                except Exception as e:
+                    print(f"Error in GPS polling: {e}")
+                    break
 
 with socketserver.TCPServer(("", PORT), CustomHTTPRequestHandler) as httpd:
     print(f"Server started at localhost:{PORT}")
