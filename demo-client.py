5f840da9126876c87b302c5136715225ba79b324
Jacob Funk <jacobstevenfunk@gmail.com>
2023-12-01 10:18:36 -0800

    finalized demo code

---
+++ demo-client.py
@@ -0,0 +1,114 @@
+import requests
+import os
+import time
+
+raspberry_pi_ip = '10.42.0.1'  # Raspberry Pi's IP address
+port = 8000
+base_url = f'http://{raspberry_pi_ip}:{port}'
+counter_url = f'{base_url}/counter'  # For counter requests
+pre_processed_image_url = f'{base_url}/pre-processed-image'
+post_processed_image_url = f'{base_url}/post-processed-image'
+
+csv_url = f'{base_url}/csv'
+take_picture_url = f'{base_url}/take-picture'
+gps_poll_url = f'{base_url}/gps-poll'  # For GPS polling
+gps_stop_url = f'{base_url}/gps-stop'
+
+cv_url = f'{base_url}/computer-vision'
+
+desktop_path = os.path.expanduser('~/Desktop/demo-files')
+pre_image_path = os.path.join(desktop_path, 'pre-processed-image.jpeg')
+post_image_path = os.path.join(desktop_path, 'post-processed-image.jpeg')
+
+csv_path = os.path.join(desktop_path, 'GPS_Data.csv')
+
+def request_computer_vision():
+    try:
+        response = requests.get(cv_url)
+        print(f"Running through Model")
+        if response.status_code == 200:
+            print("Successful running model")
+    except Exception as e:
+        print(f"Error requesting CV: {e}")
+
+def request_pre_image():
+    try:
+        response = requests.get(pre_processed_image_url)
+        if response.status_code == 200:
+            with open(pre_image_path, 'wb') as file:
+                file.write(response.content)
+            print(f"Image saved as '{pre_image_path}'")
+        else:
+            print("Failed to retrieve image")
+    except Exception as e:
+        print(f"Error requesting image: {e}")
+
+def request_post_image():
+    try:
+        response = requests.get(post_processed_image_url)
+        if response.status_code == 200:
+            with open(post_image_path, 'wb') as file:
+                file.write(response.content)
+            print(f"Image saved as '{post_image_path}'")
+        else:
+            print("Failed to retrieve image")
+    except Exception as e:
+        print(f"Error requesting image: {e}")
+
+def request_csv():
+    try:
+        response = requests.get(csv_url)
+        if response.status_code == 200:
+            with open(csv_path, 'wb') as file:
+                file.write(response.content)
+            print(f"CSV file saved as '{csv_path}'")
+        else:
+            print("Failed to retrieve CSV file")
+    except Exception as e:
+        print(f"Error requesting CSV file: {e}")
+
+def request_take_picture():
+    try:
+        response = requests.get(take_picture_url)
+        print("Taking Photo")
+        if response.status_code == 200:
+            print("Photo taken success")
+    except Exception as e:
+        print(f"Error taking photo: {e}")
+        
+def request_gps_poll():
+    try:
+        response = requests.get(gps_poll_url)
+        print("GPS polling started.")
+    except Exception as e:
+        print(f"Error starting GPS polling: {e}")
+
+def stop_gps_polling():
+    try:
+        response = requests.get(gps_stop_url)
+        print("GPS polling stopped.")
+    except Exception as e:
+        print(f"Error stopping GPS polling: {e}")
+
+def main():
+    commands = {
+        'take_picture': request_take_picture,
+        'gps_poll': request_gps_poll,
+        'pre_image': request_pre_image,
+        'computer_vision': request_computer_vision,
+        'post_image': request_post_image,
+        'csv': request_csv,
+        'stop_gps': stop_gps_polling
+    }
+
+    while True:
+        user_input = input("Enter command: ")
+        if user_input in commands:
+            commands[user_input]()
+        elif user_input == 'exit':
+            break
+        else:
+            print("Unknown command")
+
+if __name__ == "__main__":
+    main()
---
+++ demo-server.py
@@ -0,0 +1,127 @@
+import http.server
+import socketserver
+import os
+import threading
+import time
+import csv
+import cv2 as cv
+from picamera2 import Picamera2, Preview
+
+PORT = 8000
+pre_image_path = os.path.expanduser('~/Desktop/server-client/pre-processed-image.jpeg')
+post_image_path = os.path.expanduser('~/Desktop/server-client/post-processed-image.jpeg')
+
+modelConfig = os.path.expanduser('~/Desktop/Object_Detection_Files/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt')
+modelWeights = os.path.expanduser('~/Desktop/Object_Detection_Files/frozen_inference_graph.pb')
+modelClassList = os.path.expanduser('~/Desktop/Object_Detection_Files/coco_names.txt')
+
+csv_path = os.path.expanduser('~/Desktop/GPS_Data.csv')
+
+gps_polling_active = False
+stop_gps_polling = False
+
+picam2 = Picamera2()
+camera_config = picam2.create_preview_configuration()
+picam2.configure(camera_config)
+picam2.start_preview(Preview.NULL)
+picam2.start()
+
+model = cv.dnn_DetectionModel(modelWeights, modelConfig)
+model_confidence_threshold = 0.65
+model.setInputSize(320, 320)
+model.setInputScale(1.0/127.5)
+model.setInputMean((127.5, 127.5, 127.5))
+model.setInputSwapRB(True)  
+
+#make list of class labels from dataset (only used when drawing box titles)
+with open(modelClassList, 'rt') as spt:
+    classLabels = spt.read().rstrip('\n').split('\n')
+
+class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        global gps_polling_active
+        if self.path == '/pre-processed-image':
+            #edit the image to send back the image either pre or post processing
+            self.serve_file(pre_image_path, 'image/jpeg')
+        elif self.path =='/post-processed-image':
+            self.serve_file(post_image_path, 'image/jpeg')
+        elif self.path == '/csv':
+            self.serve_file(csv_path, 'text/csv')
+        elif self.path == '/take-picture':
+            self.take_picture()
+        elif self.path =='/gps-poll':
+            gps_polling_active = True
+            self.serve_gps_poll()
+        elif self.path =='/gps-stop':
+            self.stop_gps_polling()
+        elif self.path == '/computer-vision':
+            self.computer_vision()
+            #call function to run model on image
+            #create image in directory called post-processed-image.jpeg
+        else:
+            self.serve_not_found()
+
+    def serve_file(self, file_path, content_type):
+        try:
+            with open(file_path, 'rb') as file:
+                self.send_response(200)
+                self.send_header('Content-type', content_type)
+                self.end_headers()
+                self.wfile.write(file.read())
+        except FileNotFoundError:
+            self.serve_not_found()
+
+    def serve_not_found(self):
+        self.send_response(404)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"Resource not found.")
+
+    def take_picture(self):
+        picam2.capture_file("pre-processed-image.jpeg")
+        self.send_response(200) 
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"Photo taken")
+    
+    def poll_gps(self):
+        global stop_gps_polling
+        while not stop_gps_polling:
+            print("Waiting for a valid GPS fix...")
+            time.sleep(1)
+    
+    def serve_gps_poll(self):
+        global stop_gps_polling
+        stop_gps_polling = False
+        thread = threading.Thread(target=self.poll_gps)
+        thread.start()
+        self.send_response(200) 
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"GPS polling started...")
+        
+    def stop_gps_polling(self):
+        global stop_gps_polling, gps_polling_active
+        stop_gps_polling = True
+        gps_polling_active = False
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"GPS polling stopped.")
+    
+    def computer_vision(self):
+        img = cv.imread(pre_image_path)
+        classIndex, confidence, bbox = model.detect(img, confThreshold=model_confidence_threshold, nmsThreshold=0.2) 
+        for classInd, conf, boxes in zip(classIndex, confidence, bbox):
+            cv.rectangle(img, boxes, (0, 255, 0), 1)
+            cv.putText(img, classLabels[classInd-1], (boxes[0] + 10, boxes[1] + 20), cv.FONT_HERSHEY_PLAIN, fontScale = 1, color=(0, 0, 255), thickness=2)
+        cv.imwrite(post_image_path, img)
+        self.send_response(200) 
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"Computer Vision finished")
+        
+
+with socketserver.TCPServer(("", PORT), CustomHTTPRequestHandler) as httpd:
+    print(f"Server started at localhost:{PORT}")
+    httpd.serve_forever()
