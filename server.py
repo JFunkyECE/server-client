7104842f659d90481d4d07f450dea40d5d4bb2bd
Jacob Funk <jacobstevenfunk@gmail.com>
2023-11-30 16:35:51 -0800

    added structure for implementing CV and picamera functionality to server-client program

--- server.py
+++ server.py
@@ -18,6 +18,7 @@ class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         global gps_polling_active
 
         if self.path == '/image':
+            #edit the image to send back the image either pre or post processing
             self.serve_file(image_path, 'image/jpeg')
         elif self.path == '/csv':
             self.serve_file(csv_path, 'text/csv')
@@ -26,12 +27,16 @@ class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
                 gps_polling_active = True
                 self.serve_gps_poll()
             else:
-                self.send_response(200)
+                self.send_response(200) #200 means ok
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 self.wfile.write(b"GPS polling is already active.")
         elif self.path == '/gps-stop':
             self.stop_gps_polling()
+        elif self.path == '/take-picture':
+            #call function for taking photo on rpi
+        elif self.path == '/computer-vision':
+            #call function to run model on image
         else:
             self.serve_not_found()
 
@@ -56,7 +61,7 @@ class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         stop_gps_polling = False
         thread = threading.Thread(target=self.poll_gps)
         thread.start()
-        self.send_response(200)
+        self.send_response(200) 
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         self.wfile.write(b"GPS polling started...")
